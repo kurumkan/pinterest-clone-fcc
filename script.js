bd17448fd9936bd40e4c846b0fff41ffbc476b60
Artur Arsalanov <joomrise@gmail.com>
2017-09-04 22:34:24 +0300

    fix grid positioning

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ var navigationTop = document.getElementById('navigation-top');
 searchBtn.onclick = function() {
     navigationTop.classList.toggle('open');
 }
+
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+    // options
+    horizontalOrder: true,
+    itemSelector: '.grid-item',
+    columnWidth: '.grid-sizer',
+    gutter: 5,
+    fitWidth: true
+
+
+});
