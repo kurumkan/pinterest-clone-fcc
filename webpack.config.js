632c5ee64f3605d7ea14ccc9386b5b5e9fc3e51f
Artur Arsalanov <joomrise@gmail.com>
2017-10-02 10:30:11 +0300

    add code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
 const webpack = require('webpack');
 const path = require('path');
 
+const VENDOR_LIBS = [
+  'react'
+];
+
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: '[name].[chunkhash].js'
@@ -19,6 +26,16 @@ module.exports = {
   },
 
   plugins: [
-
+    new webpack.optimize.CommonsChunkPlugin({
+      names: [
+        'vendor', // if there are some common code(between vendor and bundle) - place it in vendor as well
+        'manifest' // map all the modules and bundle files
+      ]
+    }),
+    new webpack.DefinePlugin({ // allows you to create global constants, they can be set at compile time
+      // on the left setting NODE_ENV - global variable(window-scope)
+      // on the right NODE_ENV - can be set at compile time
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
   ]
 };
