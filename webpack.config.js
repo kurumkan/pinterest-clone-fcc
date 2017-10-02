642ad1f155685397b8c2fc8287987e72f67703d2
Artur Arsalanov <joomrise@gmail.com>
2017-10-02 10:47:14 +0300

    add image preprocessing support

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,23 @@ module.exports = {
           fallback: 'style-loader', // attach css to DOM (within <style></style> tag)
           use: 'css-loader' // transofm import & url(...) from css to require(...)
         })
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              // if the file greater than limit put it into build directory
+              // else include it into bundle.js file
+              limit: 10000, // bytes,
+              fallback: { // if url-loader fails - use fallback loader
+                loader: 'file-loader' // loads files
+              }
+            }
+          },
+          'image-webpack-loader' // optimize image size
+        ]
       }
     ]
   },
@@ -42,7 +59,7 @@ module.exports = {
       ]
     }),
     new ExtractTextPlugin("styles.css"), // extract text from bundle into sepatate file,
-    new HtmlWebpackPlugin({ // generate html file for us from template + add scripts to that
+    new HtmlWebpackPlugin({ // generate html file for us from template + add scripts & styles to that
       template: 'src/index.html'
     }),
     new webpack.DefinePlugin({ // allows you to create global constants, they can be set at compile time
