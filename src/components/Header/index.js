b3fb4ca422e1ed896112005c8a05e187f73bc307
Artur Arsalanov <joomrise@gmail.com>
2017-10-02 17:09:30 +0300

    fix HotModuleReplacement issues

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,43 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Header = () => <div>Header</div>;
+import './Header.css';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <nav className="Header">
+        <div className="logo">
+          <a href="#"><img src="images/logo.jpg" alt="Logo" /></a>
+        </div>
+        <div className="search-btn-wrapper">
+          <span className="btn search-btn">
+              <i className="fa fa-search" aria-hidden="true"></i>
+          </span>
+        </div>
+        <div className="search-wrapper">
+          <div className="search-container">
+            <i className="icon left fa fa-search" aria-hidden="true"></i>
+            <input type="search" name="top-search" placeholder="Search"/>
+          </div>
+        </div>
+        <div className="home-btn-wrapper">
+          <a href="#" className="btn home-btn">
+            Home
+          </a>
+        </div>
+        <div className="profile-btn-wrapper">
+          <a href="#" className="btn profile-btn">
+            <i className="icon rounded fa fa-user" aria-hidden="true"></i>
+            <span className="username">ARTURARTUR</span>
+          </a>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Header;
--- webpack.config.js
+++ webpack.config.js
@@ -9,14 +9,14 @@ const VENDOR_LIBS = [
 
 const isDev = process.env.NODE_ENV !== 'production';
 
-module.exports = {
+const webpackConfig = {
   entry: {
     bundle: './src/index.js',
     vendor: VENDOR_LIBS
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: `[name]${isDev ? '' : '[chunkhash:8]'}.js` // chunkhash - a unique string generated based on the file content
+    filename: `[name]${isDev ? '' : '[chunkhash]'}.js` // chunkhash - a unique string generated based on the file content
     // chunk hash should not be used for development - can cause memory leak
   },
 
@@ -29,7 +29,10 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ExtractTextPlugin.extract({
+        use: isDev ? [
+          'style-loader',
+          'css-loader'
+        ] : ExtractTextPlugin.extract({
           fallback: 'style-loader', // attach css to DOM (within <style></style> tag)
           use: 'css-loader' // transofm import & url(...) from css to require(...)
         })
@@ -68,17 +71,15 @@ module.exports = {
         'manifest' // map all the modules and bundle files
       ]
     }),
-    new ExtractTextPlugin("styles.css"), // extract text from bundle into sepatate file,
-    new HtmlWebpackPlugin({ // generate html file for us from template + add scripts & styles to that
-      template: 'src/index.html'
-    }),
     new webpack.DefinePlugin({ // allows you to create global constants, they can be set at compile time
       // on the left setting NODE_ENV - global variable(window-scope)
       // on the right NODE_ENV - can be set at compile time
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
     }),
-    new webpack.HotModuleReplacementPlugin({
-      multiStep: true
+
+    new HtmlWebpackPlugin({ // generate html file for us from template + add scripts to that
+      template: 'src/index.html',
+      title: 'Pinterest Clone'
     })
   ],
 
@@ -97,3 +98,18 @@ module.exports = {
     contentBase: './'
   }
 };
+
+// in production
+if(!isDev) {
+  webpackConfig.plugins.push(
+    new ExtractTextPlugin("styles.css") // extract text from bundle into sepatate file
+  );
+} else {
+  webpackConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin({
+      multiStep: false
+    })
+  );
+}
+
+module.exports = webpackConfig;
