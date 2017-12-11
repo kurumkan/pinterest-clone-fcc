30c83f181a993c5379646891ead557e96902e8d7
Artur Arsalanov <joomrise@gmail.com>
2017-12-11 13:57:38 +0300

    remove Modal

--- src/components/ImageGridItem/index.js
+++ src/components/ImageGridItem/index.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 import { Link } from 'react-router';
 import Tooltip from 'components/Tooltip';
-import Modal from 'components/Modal';
 
 import { GridItem } from 'react-masonry-infinite-scroll';
 
@@ -12,10 +11,8 @@ import './ImageGridItem.css';
 class ImageGridItem extends Component {
   constructor(props) {
     super(props);
-    this.state = { showModal: false };
     this.handleShareBtnClick = this.handleShareBtnClick.bind(this);
     this.handleClickOutside = this.handleClickOutside.bind(this);
-    this.toggleModal = this.toggleModal.bind(this);
     this.handleSaveClick = this.handleSaveClick.bind(this);
   }
 
@@ -25,12 +22,6 @@ class ImageGridItem extends Component {
 
   componentWillUnmount() {
     document.removeEventListener('mousedown', this.handleClickOutside);
-  }
-
-  toggleModal() {
-    this.setState({
-      showModal: !this.state.showModal
-    });
   }
 
   handleShareBtnClick(e) {
@@ -52,43 +43,36 @@ class ImageGridItem extends Component {
     const { description, author, img, shouldShowPopup } = this.props;
     return (
       <GridItem className={`${shouldShowPopup && 'focused'}`}>
-        <div onClick={this.toggleModal}>
-          <img src={ img }/>
-            <span
-              className={`btn share-btn fa fa-share-alt ${shouldShowPopup ? 'visible' : ''}`}
-              onClick={this.handleShareBtnClick}
-              ref={el => this.shareBtn = el }
+        <img src={ img }/>
+          <span
+            className={`btn share-btn fa fa-share-alt ${shouldShowPopup ? 'visible' : ''}`}
+            onClick={this.handleShareBtnClick}
+            ref={el => this.shareBtn = el }
+          >
+            <Tooltip
+              targetElement={this.shareBtn}
+              isOpen={shouldShowPopup}
+              width={190}
+              height={90}
             >
-              <Tooltip
-                targetElement={this.shareBtn}
-                isOpen={shouldShowPopup}
-                width={190}
-                height={90}
-              >
-                <span className="title">
-                  Share this Pin
-                </span>
-                <div className="button-group">
-                  <FontAwesome name="facebook" className="social-btn" />
-                  <FontAwesome name="twitter" className="social-btn" />
-                  <FontAwesome name="link" className="social-btn" />
-                </div>
-              </Tooltip>
-            </span>
-            <span className="btn save-btn" onClick={this.handleSaveClick}>
-              <FontAwesome name="thumb-tack" className="icon" />
-              Save
-            </span>
-          <p className="img-description">
-            { description } {this.props.id}
-            <Link className="author" to="#">By { author }</Link>
-          </p>
-          <Modal styles={{ padding: 10 }} show={this.state.showModal} hide={this.toggleModal}>
-            <div className="detailed-pin">
-              <img src={ img }/>
-            </div>
-          </Modal>
-        </div>
+              <span className="title">
+                Share this Pin
+              </span>
+              <div className="button-group">
+                <FontAwesome name="facebook" className="social-btn" />
+                <FontAwesome name="twitter" className="social-btn" />
+                <FontAwesome name="link" className="social-btn" />
+              </div>
+            </Tooltip>
+          </span>
+          <span className="btn save-btn" onClick={this.handleSaveClick}>
+            <FontAwesome name="thumb-tack" className="icon" />
+            Save
+          </span>
+        <p className="img-description">
+          { description } {this.props.id}
+          <Link className="author" to="#">By { author }</Link>
+        </p>
       </GridItem>
     );
   }
