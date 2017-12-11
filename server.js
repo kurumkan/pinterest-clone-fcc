768a0bdbabf875ab7ac4daf614954a6bd5c15aca
Artur Arsalanov <joomrise@gmail.com>
2017-12-11 14:50:15 +0300

    replace mock images

--- server.js
+++ server.js
@@ -6,115 +6,115 @@ app.get('/api/pins', (req, res) => {
   const pins = [
     {
       id: '0',
-      img: 'src/images/350.jpeg',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '1',
-      img: 'src/images/200x400.jpeg',
+      img: 'http://www.newportharborvets.com/sites/default/files/08-cat-cancer-4.jpeg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '2',
-      img: 'src/images/350.jpeg',
+      img: 'https://www.aspca.org/sites/default/files/team-aspca-birthday-campaign-ad_090516_cat.jpg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '3',
-      img: 'src/images/300.jpeg',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '4',
-      img: 'src/images/350.jpeg',
+      img: 'http://www.newportharborvets.com/sites/default/files/08-cat-cancer-4.jpeg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '5',
-      img: 'src/images/900x900.jpeg',
+      img: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-03/3/16/enhanced/webdr10/enhanced-19898-1425418851-9.jpg?downsize=715:*&output-format=auto&output-quality=auto',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '6',
-      img: 'src/images/350.jpeg',
+      img: 'https://www.aspca.org/sites/default/files/team-aspca-birthday-campaign-ad_090516_cat.jpg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '7',
-      img: 'src/images/300.jpeg',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '8',
-      img: 'src/images/350.jpeg',
+      img: 'http://www.newportharborvets.com/sites/default/files/08-cat-cancer-4.jpeg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '9',
-      img: 'src/images/300.jpeg',
+      img: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-03/3/16/enhanced/webdr10/enhanced-19898-1425418851-9.jpg?downsize=715:*&output-format=auto&output-quality=auto',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '10',
-      img: 'src/images/350.jpeg',
+      img: 'https://www.aspca.org/sites/default/files/team-aspca-birthday-campaign-ad_090516_cat.jpg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '11',
-      img: 'src/images/300.jpeg',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '12',
-      img: 'src/images/350.jpeg',
+      img: 'http://www.newportharborvets.com/sites/default/files/08-cat-cancer-4.jpeg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '13',
-      img: 'src/images/300.jpeg',
+      img: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-03/3/16/enhanced/webdr10/enhanced-19898-1425418851-9.jpg?downsize=715:*&output-format=auto&output-quality=auto',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '14',
-      img: 'src/images/350.jpeg',
+      img: 'https://www.aspca.org/sites/default/files/team-aspca-birthday-campaign-ad_090516_cat.jpg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '15',
-      img: 'src/images/300.jpeg',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/1200px-Cat_November_2010-1a.jpg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '16',
-      img: 'src/images/350.jpeg',
+      img: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-03/3/16/enhanced/webdr10/enhanced-19898-1425418851-9.jpg?downsize=715:*&output-format=auto&output-quality=auto',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '17',
-      img: 'src/images/300.jpeg',
+      img: 'https://img.buzzfeed.com/buzzfeed-static/static/2015-03/3/16/enhanced/webdr10/enhanced-19898-1425418851-9.jpg?downsize=715:*&output-format=auto&output-quality=auto',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     },
     {
       id: '18',
-      img: 'src/images/350.jpeg',
+      img: 'https://www.aspca.org/sites/default/files/team-aspca-birthday-campaign-ad_090516_cat.jpg',
       author: 'Ethan Hein',
       description: 'Lorem Ipsum'
     }
